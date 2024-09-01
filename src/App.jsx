7ae9481cd3e66e1def0959223ff930758b229063
Borngod <aboa.boni360@gmail.com>
2024-09-01 23:59:42 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Button} from './components/ui/button'
-import {Outlet,Navigate} from 'react-router-dom'
-import Header from './components/custom/Header'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { Outlet, Navigate } from "react-router-dom";
+import Header from "./components/custom/Header";
+import { useUser } from "@clerk/clerk-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const {user,isloaded,isSignedIn}= useState();
+  const [count, setCount] = useState(0);
+  const { user, isloaded, isSignedIn } = useUser();
 
   if (!isloaded) {
     return <div>Loading...</div>;
   }
 
-  if(!isSignedIn)
-  {
-    return <Navigate to={'/auth/sign-in'} />
+  if (!isSignedIn) {
+    return <Navigate to={"/auth/sign-in"} />;
   }
 
   return (
     <>
-     {/* <Header/>
-     <Outlet/> */}
-     <p>i am eugeniiiiiiiii</p>
+      <Header />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
